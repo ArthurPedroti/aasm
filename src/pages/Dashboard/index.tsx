19c91e32aa562faa9c3ebb79f252eb01c2c79b76
Arthur Pedroti <arthurpedroti@gmail.com>
2020-07-27 22:36:04 -0300

    feat: dashboard page style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from '../../hooks/auth';
 
@@ -9,10 +9,36 @@ import {
   HeaderTitle,
   UserName,
   ProvidersList,
+  ProviderContainer,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderMetaText,
+  ProvidersListTitle,
 } from './styles';
 
+export interface Provider {
+  name: string;
+  type: string;
+}
+
+const providers = [
+  {
+    name: 'Arthur',
+    type: 'with-palliative-solution',
+  },
+  {
+    name: 'João',
+    type: 'without-palliative-solution',
+  },
+  {
+    name: 'Lucas',
+    type: 'critical-without-palliative-solution',
+  },
+];
+
 const Dashboard: React.FC = () => {
-  const { signOut, user } = useAuth();
+  const { user } = useAuth();
   return (
     <Container>
       <Header>
@@ -22,7 +48,46 @@ const Dashboard: React.FC = () => {
         </HeaderTitle>
       </Header>
 
-      <ProvidersList />
+      <ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.name}
+        ListHeaderComponent={
+          <ProvidersListTitle>Seus chamados</ProvidersListTitle>
+        }
+        renderItem={({ item: provider }) => (
+          <ProviderContainer onPress={() => {}}>
+            <Icon name="settings" size={72} color="#f7df1e" />
+
+            <ProviderInfo>
+              <ProviderName>{provider.name}</ProviderName>
+              <ProviderMeta>
+                <Icon name="calendar" size={14} color="#999591" />
+                <ProviderMetaText>Assistencia técnica</ProviderMetaText>
+              </ProviderMeta>
+              {provider.type === 'with-palliative-solution' ? (
+                <ProviderMeta>
+                  <Icon name="alert-circle" size={14} color="#e6fffa" />
+                  <ProviderMetaText>Com solução paliativa</ProviderMetaText>
+                </ProviderMeta>
+              ) : null}
+              {provider.type === 'without-palliative-solution' ? (
+                <ProviderMeta>
+                  <Icon name="alert-triangle" size={14} color="#f7df1e" />
+                  <ProviderMetaText>Sem solução paliativa</ProviderMetaText>
+                </ProviderMeta>
+              ) : null}
+              {provider.type === 'critical-without-palliative-solution' ? (
+                <ProviderMeta>
+                  <Icon name="alert-octagon" size={14} color="#c53030" />
+                  <ProviderMetaText>
+                    Crítico e sem solução paliativa
+                  </ProviderMetaText>
+                </ProviderMeta>
+              ) : null}
+            </ProviderInfo>
+          </ProviderContainer>
+        )}
+      />
     </Container>
   );
 };
