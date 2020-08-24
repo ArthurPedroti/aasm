bfa0a6064f2cb2645ce91d5a1b4772d24db0a690
Arthur Pedroti <arthurpedroti@gmail.com>
2020-08-24 16:47:22 -0300

    feat: environment variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_URL } from 'react-native-dotenv';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: API_URL,
 });
 
 export default api;
