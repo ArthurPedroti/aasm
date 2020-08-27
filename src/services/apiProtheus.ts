7d139b0311b093ad8d116ccfcf935021a8b1d2c2
Arthur Pedroti <arthurpedroti@gmail.com>
2020-08-27 10:42:46 -0300

    fix: env variable at protheusapi

--- src/services/apiProtheus.ts
+++ src/services/apiProtheus.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_PROTHEUS_URL } from 'react-native-dotenv';
 
 const apiProtheus = axios.create({
-  baseURL: 'http://192.168.2.250:9001',
+  baseURL: API_PROTHEUS_URL,
 });
 
 export default apiProtheus;
