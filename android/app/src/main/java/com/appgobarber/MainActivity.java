6ca76a34e8743a5cd4a81e34a57e8750827e8c40
Arthur Pedroti <arthurpedroti@gmail.com>
2020-08-31 09:43:06 -0300

    refactor: android folder

---
+++ android/app/src/main/java/com/appgobarber/MainActivity.java
@@ -0,0 +1,19 @@
+package com.appgobarberatp;
+
+import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // Adicione esse import
+import android.os.Bundle; // Adicione esse import
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  protected String getMainComponentName() {
+    return "appgobarber";
+  }
+}
