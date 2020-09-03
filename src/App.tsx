754426dbc8380973ef13e2df50d599bc8d87e585
Arthur Pedroti <arthurpedroti@gmail.com>
2020-09-03 17:50:58 -0300

    feat: one signal configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import 'react-native-gesture-handler';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, Component } from 'react';
 import { View, StatusBar } from 'react-native';
 import codePush from 'react-native-code-push';
 import { NavigationContainer } from '@react-navigation/native';
 import SplashScreen from 'react-native-splash-screen';
+import OneSignal from 'react-native-onesignal';
 
 import AppProvider from './hooks';
 
 import Routes from './routes';
 
-const App: React.FC = () => {
+export interface Notification {
+  payload: {
+    additionalData: {
+      user_id: string;
+    };
+  };
+}
+
+const AppToOneSignal: React.FC = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
@@ -26,6 +35,55 @@ const App: React.FC = () => {
     </NavigationContainer>
   );
 };
+
+class App extends Component {
+  constructor(properties: any) {
+    super(properties);
+    // Remove this method to stop OneSignal Debugging
+    OneSignal.setLogLevel(6, 0);
+
+    // Replace 'YOUR_ONESIGNAL_APP_ID' with your OneSignal App ID.
+    OneSignal.init('7e3436d4-7ce1-40e2-ad3c-5f99e92130cb', {
+      kOSSettingsKeyAutoPrompt: false,
+      kOSSettingsKeyInAppLaunchURL: false,
+      kOSSettingsKeyInFocusDisplayOption: 2,
+    });
+    OneSignal.inFocusDisplaying(2); // Controls what should happen if a notification is received while the app is open. 2 means that the notification will go directly to the device's notification center.
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification: Notification): void {
+    console.log('Data received: ', notification.payload.additionalData.user_id);
+    console.log('Data received: ', notification.payload.notificationID);
+    console.log('Notification received: ', notification);
+    OneSignal.cancelNotification(notification.payload.notificationID);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
+  render() {
+    return <AppToOneSignal />;
+  }
+}
+
 export default codePush({
   checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
 })(App);
